cccbd4368c7b8a2eb5292f153951f7787fa628b8
mikaildokgoz <mikaildokgoz@gmail.com>
2022-04-20 16:03:54 +0300

    v4

--- src/App.js
+++ src/App.js
@@ -7,11 +7,72 @@ import woman from "../src/assets/woman.svg";
 import phone from "../src/assets/phone.svg";
 import padlock from "../src/assets/padlock.svg";
 import map from "../src/assets/map.svg";
-import useFetch from "./customHook/useFetch";
+import { useEffect, useState } from "react";
 
 function App() {
-  const { data } = useFetch("https://randomuser.me/api/");
-  console.log(data);
+  const [data, setData] = useState();
+  const [error, setError] = useState(false);
+  const [showTable, setShowTable] = useState(false);
+  const [up, setUp] = useState("name");
+  const [down, setDown] = useState("");
+
+  const getApi = () => {
+    fetch("https://randomuser.me/api/")
+      .then((res) => {
+        if (!res.ok) {
+          setError("Something went wrong!!!");
+          setData([]);
+        } else {
+          return res.json();
+        }
+      })
+      .then((res) => {
+        setData(res.results[0]);
+        setError("");
+      });
+  };
+
+  useEffect(() => {
+    getApi();
+    setDown(data?.name.first);
+  }, []);
+
+  const handleRandom = () => {
+    getApi();
+    setDown(data?.name.first);
+  };
+
+  const handleAdd = () => {
+    setShowTable(true);
+
+  };
+
+  const handleOver = (e) => {
+      if(e.target.id === "1" || e.target.id === "2" ){
+        setUp("name")
+        setDown(data?.name.first)
+      }
+      else if(e.target.id === "3" ){
+        setUp("mail")
+        setDown(data?.email)
+      }
+      else if(e.target.id === "4" || e.target.id === "5" ){
+        setUp("age")
+        setDown(data?.dob.age)
+      }
+      else if(e.target.id === "6" ){
+        setUp("map")
+        setDown(data?.email)
+      }
+      else if(e.target.id === "7" ){
+        setUp("mail")
+        setDown(data?.email)
+      }
+      else if(e.target.id === "8" ){
+        setUp("mail")
+        setDown(data?.email)
+      }
+  }
 
   return (
     <div className="App">
@@ -19,33 +80,49 @@ function App() {
         <img src={data?.picture.large} alt="" />
       </div>
       <div className="description">
-        <h2></h2>
-        <h2></h2>
+        <h2>My {up} is</h2>
+        <h2>{down}</h2>
       </div>
       <div className="icons">
         <div>
-          <img className="icon" id="1" src={man} alt="" />
+          <img className="icon" id="1" src={man} alt="" onMouseOver={handleOver} />
         </div>
         <div>
-          <img className="icon" id="3" src={mail} alt="" />
+          <img className="icon" id="3" src={mail} alt="" onMouseOver={handleOver} />
         </div>
         <div>
-          <img className="icon" id="4" src={oldMan} alt="" />
+          <img className="icon" id="4" src={oldMan} alt="" onMouseOver={handleOver}/>
         </div>
         <div>
-          <img className="icon" id="6" src={map} alt="" />
+          <img className="icon" id="6" src={map} alt="" onMouseOver={handleOver} />
         </div>
         <div>
-          <img className="icon" id="7" src={phone} alt="" />
+          <img className="icon" id="7" src={phone} alt="" onMouseOver={handleOver} />
         </div>
         <div>
-          <img className="icon" id="8" src={padlock} alt="" />
+          <img className="icon" id="8" src={padlock} alt="" onMouseOver={handleOver} />
         </div>
       </div>
       <div className="buttons">
-        <button>Random User</button>
-        <button>Add User</button>
+        <button onClick={handleRandom}>Random User</button>
+        <button onClick={handleAdd}>Add User</button>
       </div>
+      {showTable && (
+        <div className="table">
+          <table>
+            <thead>
+              <th>First Name</th>
+              <th>Age</th>
+              <th>Phone</th>
+            </thead>
+            <tbody>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 }
